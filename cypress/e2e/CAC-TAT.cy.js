4cbb447c64d634aa195dede7c04347d6a264b682
starline.leonardo <starline.leonardo@ituran.com.br>
2024-07-09 00:37:05 -0300

    Required fieds validate

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,23 @@
 
-describe('Central de Atendimento ao Cliente', ()=> {
-   it('verificar o titulo da aplicação', () => {
-       cy.visit('././src/index.html')
-       cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-   })
+describe('Central de Atendimento ao Cliente', () => {
+  beforeEach(() => {
+    cy.visit('././src/index.html')
+  })
+  it('verificar o titulo da aplicação', () => {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+  it.only('Prencher campos obrigatórios e enviar formulário', () => {
+    cy.get('#firstName').type('Leonardo Henrique')
+    cy.get('#lastName').type('Luz Marciano')
+    cy.get('#email').type('Leo@teste.com')
+    cy.get('#phone').type('11 9999-9999')
+    cy.get('#open-text-area').type('Cypress é top!')
+    cy.get('#open-text-area').type('Lorem ipsum dolor sit amet. Sed saepe quia qui recusandae perspiciatis et repellendus sint! Qui porro cumque cum fugiat dignissimos qui sapiente sint non reiciendis dolorum in placeat quibusdam. Eum commodi voluptatem ea totam Quis sed quia voluptatibus ad tempora officia non quisquam provident aut internos dolorem ut magni repudiandae.', 
+      {delay: 0})
+    cy.get('button[type = "submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
+
 })
+
